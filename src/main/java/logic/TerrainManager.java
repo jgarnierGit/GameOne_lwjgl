f498fdc7adaa170dda1c21cc8392657010f3f119
Jonathan G <jonat.garnier@free.fr>
2020-06-19 01:09:32 +0200

    refactor: use static ThreadLocalRandom from java 7 instead of instanciated Random. better api.

--- src/main/java/logic/TerrainManager.java
+++ src/main/java/logic/TerrainManager.java
@@ -2,7 +2,7 @@ package logic;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 import org.lwjgl.glfw.GLFW;
 import org.lwjgl.opengl.GL11;
@@ -20,12 +20,10 @@ import renderEngine.RenderingParameters;
 public class TerrainManager extends InputInteractable implements EntityContainer{
 	List<Terrain3D> terrains = new ArrayList<>();
 	MasterRenderer masterRenderer;
-	Random random;
 	
 	private TerrainManager(MasterRenderer masterRenderer,PlayerInputListener inputListener) {
 		super(inputListener);
 		this.masterRenderer = masterRenderer;
-		random = new Random();
 	}
 	
 	public static TerrainManager create(MasterRenderer masterRenderer,PlayerInputListener inputListener) {
@@ -43,9 +41,9 @@ public class TerrainManager extends InputInteractable implements EntityContainer
 	}
 	
 	public void addTerrain() {
-		float x = random.nextFloat() * 50;
-		float y = random.nextFloat() * 100; //elevation
-		float z = random.nextFloat() * 50;
+		float x = (float) ThreadLocalRandom.current().nextDouble(0, 100);
+		float y = (float) ThreadLocalRandom.current().nextDouble(-10, 30); //elevation
+		float z = (float) ThreadLocalRandom.current().nextDouble(-30, 30);
 		terrains.get(0).getRenderingParameters().addEntity(new Vector3f(x,y,z), 0, 0, 0, 1);
 		prepareForRender();
 	}
@@ -73,7 +71,7 @@ public class TerrainManager extends InputInteractable implements EntityContainer
 		// TODO hide from this interface.
 		//terrainParameters.disableRenderOptions();
 		terrainParameters.setRenderMode(GL11.GL_TRIANGLES);
-		//this.invertNormals();
+		terrain.invertNormals();
 	}
 
 	@Override
